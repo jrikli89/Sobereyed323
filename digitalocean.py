845518b4c61c8d8694054621f75286a236f506de
MutableAI <mutableai@github.com>
2023-10-08 16:49:08 +0000

    null

--- digitalocean.py
+++ digitalocean.py
@@ -1,34 +1,17 @@
 import requests
 import json
-import os
-from typing import Union, List
 
-def get_droplets() -> Union[List[dict], None]:
-    """
-    This function makes a GET request to the DigitalOcean API to retrieve a list of droplets.
-    :return: List of droplets if the API call is successful; None otherwise
-    """
-    
-    api_key = os.getenv('DO_API_KEY')
-    
-    if not api_key:
-        print("Missing API key.")
-        return None
-
+def get_droplets(api_key):
     headers = {
-        'Authorization': f'Bearer {api_key}',
+        'Authorization': f'Bearer {dop_v1_f14e98d64199585c9df696e6b2e49cc574799438799600ec0ad8beda0d933281}',
         'Content-Type': 'application/json',
     }
 
-    try:
-        response = requests.get('https://api.digitalocean.com/v2/droplets', headers=headers)
+    response = requests.get('https://api.digitalocean.com/v2/droplets', headers=headers)
 
-        if response.status_code == 200:
-            droplets = json.loads(response.text)['droplets']
-            return droplets
-        else:
-            print(f"Error: {response.status_code}")
-            return None
-    except (requests.RequestException, KeyError) as error:
-        print(f"Error in get_droplets: {error}")
+    if response.status_code == 200:
+        droplets = json.loads(response.text)['droplets']
+        return droplets
+    else:
+        print(f"Error: {response.status_code}")
         return None
