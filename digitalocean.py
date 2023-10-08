7cdb87a8baed160086dd51c780c5815060fe86ff
Sourcery AI <>
2023-10-08 03:15:07 +0000

    'Refactored by Sourcery'

--- digitalocean.py
+++ digitalocean.py
@@ -10,8 +10,6 @@ def get_droplets(api_key):
     response = requests.get('https://api.digitalocean.com/v2/droplets', headers=headers)
 
     if response.status_code == 200:
-        droplets = json.loads(response.text)['droplets']
-        return droplets
-    else:
-        print(f"Error: {response.status_code}")
-        return None
+        return json.loads(response.text)['droplets']
+    print(f"Error: {response.status_code}")
+    return None
