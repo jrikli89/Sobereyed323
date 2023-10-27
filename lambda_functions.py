75426cd045e965046bf795a505a1c43f9fadbe97
Sourcery AI <>
2023-10-27 03:23:01 +0000

    'Refactored by Sourcery'

--- lambda_functions.py
+++ lambda_functions.py
@@ -30,9 +30,7 @@ def api_call(endpoint, payload=None, method="GET"):
 # Function to register affiliate manager with proper error handling
 def register_affiliate_manager(*args, **kwargs):
     try:
-        # API integration
-        result = api_call("/register_affiliate_manager", kwargs, "POST") # replace with your API endpoint
-        return result
+        return api_call("/register_affiliate_manager", kwargs, "POST")
     except Exception as e:
         lambda_stats.increment('register_affiliate_manager.error')
         logger.error('An error occurred in register_affiliate_manager: %s', str(e))
@@ -40,9 +38,7 @@ def register_affiliate_manager(*args, **kwargs):
 # Function to monitor affiliated models with proper error handling
 def monitor_affiliated_models(*args, **kwargs):
     try:
-        # API integration
-        result = api_call("/monitor_affiliated_models", kwargs, "POST") # replace with your API endpoint
-        return result
+        return api_call("/monitor_affiliated_models", kwargs, "POST")
     except Exception as e:
         lambda_stats.increment('monitor_affiliated_models.error')
         logger.error('An error occurred in monitor_affiliated_models: %s', str(e))
@@ -50,9 +46,7 @@ def monitor_affiliated_models(*args, **kwargs):
 # Function to give credit when a new model signs up with proper error handling
 def give_credit(*args, **kwargs):
     try:
-        # API integration
-        result = api_call("/give_credit", kwargs, "POST") # replace with your API endpoint
-        return result
+        return api_call("/give_credit", kwargs, "POST")
     except Exception as e:
         lambda_stats.increment('give_credit.error')
         logger.error('An error occurred in give_credit: %s', str(e))
